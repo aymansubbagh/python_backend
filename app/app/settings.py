8d87bf959a37b82be51376cd3164d679791e8ddf
ayman <25335792+aymansubbagh@users.noreply.github.com>
2019-12-27 14:32:50 +0300

    Added custom recipe detal view.

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ol+c^qxi2e5+tq&^!z2j4m@6e56bhhaw*$3g!1$ybbkhq1iwfb'
+SECRET_KEY = 'aa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
